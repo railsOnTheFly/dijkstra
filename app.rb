5b27a53468cb207245471dac9f96462c59bb6518
saulocamposi <saulocamposi@gmail.com>
2016-03-13 23:58:40 -0300

    Basic Implementation of Dijikstra

---
+++ app.rb
@@ -0,0 +1,27 @@
+require 'dijkstra'
+
+# In this context we have 9 edges or paths.
+r = [[1, 2, 1],
+     [1, 3, 9],
+     [1, 5, 3],
+     [2, 4, 3],
+     [2, 3, 7],
+     [4, 3, 2],
+     [4, 1, 1],
+     [5, 2, 4],
+     [5, 4, 2]]
+
+start_point = 1 # starting node
+end_point = 3 # arrival node
+
+ob = Dijkstra.new(start_point, end_point, r)
+
+puts "Cost = #{ob.cost}"
+puts "Shortest Path from #{start_point} to #{end_point} = #{ob.shortest_path}"
+
+ob.write_to_file('shortestpath.out')
+
+#=>  shortestpath.out =>
+
+#    Cost -> 6
+#    Shortest Path -> [1, 2, 4, 3]
